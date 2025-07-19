f009512fb6f1194f5c8a010225e9204f504cc01d
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2025-07-19 16:18:52 +0900

    feat: TTS 모듈 추가

---
+++ LetuSpeak/Feature/Speak/Core/TextSpeaker.swift
@@ -0,0 +1,41 @@
+//
+//  TextSpeaker.swift
+//  LetuSpeak
+//
+//  Created by Importants on 7/19/25.
+//
+
+import Foundation
+import AVFAudio
+import SwiftUI
+
+@MainActor
+final class TextSpeaker: NSObject {
+    private let synthesizer = AVSpeechSynthesizer()
+    
+    override init() {
+        super.init()
+        synthesizer.delegate = self
+    }
+    
+    func speak(
+        _ text: String?,
+        utteranceSetting: CustomUtterance
+    ) throws {
+        guard let text else { throw TTSError.TextEmpty }
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = .init(identifier: utteranceSetting.voice.rawValue)
+        utterance.pitchMultiplier = utteranceSetting.pitchMultiplier
+        utterance.rate = utteranceSetting.rate
+        synthesizer.stopSpeaking(at: .immediate)
+        synthesizer.speak(utterance)
+    }
+    
+    func stop() {
+        synthesizer.stopSpeaking(at: .immediate)
+    }
+}
+
+extension TextSpeaker: AVSpeechSynthesizerDelegate {
+    
+}
