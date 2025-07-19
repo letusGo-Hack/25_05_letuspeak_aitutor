48c9f4e71310c3b70d7a9856f050eb65290900a7
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2025-07-19 16:17:27 +0900

    feat: Speak Helper 로직 추가
    추가 사항
    - Buffer Converter 로직 추가
    -  Helper 추가 (Error / Locale.Language)

---
+++ LetuSpeak/Feature/Speak/Helper/BufferConversion.swift
@@ -0,0 +1,60 @@
+/*
+Copyright © 2025 Apple Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import Foundation
+import AVFoundation
+
+class BufferConverter {
+    enum Error: Swift.Error {
+        case failedToCreateConverter
+        case failedToCreateConversionBuffer
+        case conversionFailed(NSError?)
+    }
+    
+    private var converter: AVAudioConverter?
+    func convertBuffer(_ buffer: AVAudioPCMBuffer, to format: AVAudioFormat) throws -> AVAudioPCMBuffer {
+        let inputFormat = buffer.format
+        guard inputFormat != format else {
+            return buffer
+        }
+        
+        if converter == nil || converter?.outputFormat != format {
+            converter = AVAudioConverter(from: inputFormat, to: format)
+            converter?.primeMethod = .none // Sacrifice quality of first samples in order to avoid any timestamp drift from source
+        }
+        
+        guard let converter else {
+            throw Error.failedToCreateConverter
+        }
+        
+        let sampleRateRatio = converter.outputFormat.sampleRate / converter.inputFormat.sampleRate
+        let scaledInputFrameLength = Double(buffer.frameLength) * sampleRateRatio
+        let frameCapacity = AVAudioFrameCount(scaledInputFrameLength.rounded(.up))
+        guard let conversionBuffer = AVAudioPCMBuffer(pcmFormat: converter.outputFormat, frameCapacity: frameCapacity) else {
+            throw Error.failedToCreateConversionBuffer
+        }
+        
+        var nsError: NSError?
+        var bufferProcessed = false
+        
+        let status = converter.convert(to: conversionBuffer, error: &nsError) { packetCount, inputStatusPointer in
+            defer { bufferProcessed = true } // This closure can be called multiple times, but it only offers a single buffer.
+            inputStatusPointer.pointee = bufferProcessed ? .noDataNow : .haveData
+            return bufferProcessed ? nil : buffer
+        }
+        
+        guard status != .error else {
+            throw Error.conversionFailed(nsError)
+        }
+        
+        return conversionBuffer
+    }
+}
+
--- LetuSpeak/Feature/Speak/Helper/Helpers.swift
+++ LetuSpeak/Feature/Speak/Helper/Helpers.swift
@@ -23,3 +23,48 @@ extension Recorder {
         return await AVCaptureDevice.requestAccess(for: .audio)
     }
 }
+
+
+public enum TranscriptionError: Error {
+    case couldNotDownloadModel
+    case failedToSetupRecognitionStream
+    case invalidAudioDataType
+    case localeNotSupported
+    case noInternetForModelDownload
+    case audioFilePathNotFound
+    
+    var descriptionString: String {
+        switch self {
+
+        case .couldNotDownloadModel:
+            return "Could not download the model."
+        case .failedToSetupRecognitionStream:
+            return "Could not set up the speech recognition stream."
+        case .invalidAudioDataType:
+            return "Unsupported audio format."
+        case .localeNotSupported:
+            return "This locale is not yet supported by SpeechAnalyzer."
+        case .noInternetForModelDownload:
+            return "The model could not be downloaded because the user is not connected to internet."
+        case .audioFilePathNotFound:
+            return "Couldn't write audio to file."
+        }
+    }
+}
+
+enum TTSError: Error {
+    case TTSUnavailable
+    case TextEmpty
+    
+    var message: String {
+        switch self {
+        case .TTSUnavailable: return "TTS is unvailable"
+        case .TextEmpty: return "Text is not existed"
+        }
+    }
+}
+
+public extension Locale.Language {
+    static let english = Locale.Language(identifier: "en")
+    static let korean = Locale.Language(identifier: "ko")
+}
