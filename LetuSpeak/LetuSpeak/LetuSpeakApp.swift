0f2d00f9cbf1dae84b0da720223a490949eec352
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2025-07-19 10:49:45 +0900

    init project

---
+++ LetuSpeak/LetuSpeak/LetuSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetuSpeakApp.swift
+//  LetuSpeak
+//
+//  Created by Importants on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LetuSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
