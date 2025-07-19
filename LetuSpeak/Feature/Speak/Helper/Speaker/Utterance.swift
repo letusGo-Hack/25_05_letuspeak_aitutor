38db463630f970d5228b7ebe8659612915bca1da
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2025-07-19 18:10:39 +0900

    feat: Speak 모듈 수정

--- LetuSpeak/Feature/Speak/Helper/Speaker/Utterance.swift
+++ LetuSpeak/Feature/Speak/Helper/Speaker/Utterance.swift
@@ -6,13 +6,14 @@
 //
 
 import Foundation
+internal import AVFAudio
 
-struct CustomUtterance: Sendable, Identifiable {
+struct CustomUtterance: Sendable {
     var rate: Float
     var pitchMultiplier: Float
     var postUtteranceDelay: Double
     var volume: Float
-    var voice: Voice
+    var voice: AVSpeechSynthesisVoice
     
     init(
         rate: Float = 0.5,
@@ -20,6 +21,20 @@ struct CustomUtterance: Sendable, Identifiable {
         postUtteranceDelay: Double = 1.0,
         volume: Float = 1.0,
         voice: Voice = .siri_Aaron
+    ) {
+        self.rate = rate
+        self.pitchMultiplier = pitchMultiplier
+        self.postUtteranceDelay = postUtteranceDelay
+        self.volume = volume
+        self.voice = .init(identifier: voice.rawValue)!
+    }
+    
+    init(
+        rate: Float = 0.5,
+        pitchMultiplier: Float = 1.0,
+        postUtteranceDelay: Double = 1.0,
+        volume: Float = 1.0,
+        voice: AVSpeechSynthesisVoice
     ) {
         self.rate = rate
         self.pitchMultiplier = pitchMultiplier
